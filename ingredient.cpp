a9c81438ec2ef10d62081596a2b2165dd757e261
Michel Lioronne <michel.lioronne@gmail.com>
2022-11-08 14:08:35 +0000

    New file ingredient.cpp

---
+++ ingredient.cpp
@@ -0,0 +1,26 @@
+class Ingredient {
+  public:
+    float PH = 0.;
+    float EC = 0.;
+    float dry = 0.;
+    float liquid: float = 0.;
+    float aeration: float = 0.;
+    float substrat: float = 0.;
+    float compost: float = 0.;
+    float mulch: float = 0.;
+    float carbon: float = 0.;
+    float nitrogen: float = 0.;
+    float phosphore: float = 0.;
+    float potassium: float = 0.;
+    float calcium: float = 0.;
+    float magnesium: float  = 0.;
+    float iron: float = 0.;
+    float potassium: float = 0.;
+    float molybdenium: float = 0.;
+    float copper: float = 0.;
+    float chlore: float = 0.;
+    float bore: float = 0.;
+    float silicium: float = 0.;
+    float sulfure: float = 0.;
+    Dosage recomended_dosage = None
+};
